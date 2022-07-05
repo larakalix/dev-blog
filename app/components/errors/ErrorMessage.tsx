37624a44f6288454d0a4020f493655056defdaf9
Ivan Lara <ukikalix@gmail.com>
2022-07-04 18:52:48 -0600

    Fix error message component

--- app/components/errors/ErrorMessage.tsx
+++ app/components/errors/ErrorMessage.tsx
@@ -3,7 +3,7 @@ export const ErrorMessage = ({ error }: { error: Error }) => {
         <div className="h-[80vh] flex items-center justify-center flex-col">
             <div className="border border-red-600 bg-red-400 rounded-md p-10">
                 <h1 className="text-v-main-title">Error</h1>
-                <pre className="text-v-small">{error.message}</pre>
+                <p className="text-v-small">{error.message}</p>
             </div>
         </div>
     );
